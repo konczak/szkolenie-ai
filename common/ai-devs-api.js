28e10e751ee01da5be9cc58e5067f1e5e242cba7
TTIT\konczakp <piotr.konczak@ttpsc.pl>
2023-10-31 11:04:33 +0100

    refactor: format code of ai-devs-api.js

--- common/ai-devs-api.js
+++ common/ai-devs-api.js
@@ -1,74 +1,73 @@
-
 export async function authorize(taskName, apiKey) {
-    const url = `https://zadania.aidevs.pl/token/${taskName}`;
-    const requestBody = {
-        apikey: apiKey,
-    };
+  const url = `https://zadania.aidevs.pl/token/${taskName}`;
+  const requestBody = {
+    apikey: apiKey,
+  };
 
-    try {
-        const response = await fetch(url, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(requestBody),
-        });
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(requestBody),
+    });
 
-        if (!response.ok) {
-           console.error('Ugh, sendAnswer got not ok status. Result:', await response.json());
-           throw new Error(`Network response was not ok - status ${response.status}`);
-        }
-
-        return await response.json();
-    } catch (error) {
-        console.error('Fetch error:', error);
-        throw new Error(`Ups, some error occurred during authorize: ${error.message}`);
+    if (!response.ok) {
+      console.error('Ugh, sendAnswer got not ok status. Result:', await response.json());
+      throw new Error(`Network response was not ok - status ${response.status}`);
     }
+
+    return await response.json();
+  } catch (error) {
+    console.error('Fetch error:', error);
+    throw new Error(`Ups, some error occurred during authorize: ${error.message}`);
+  }
 }
 
 export async function getTask(token) {
-    const url = `https://zadania.aidevs.pl/task/${token}`;
+  const url = `https://zadania.aidevs.pl/task/${token}`;
 
-    try {
-        const response = await fetch(url);
+  try {
+    const response = await fetch(url);
 
-        if (!response.ok) {
-           console.error('Ugh, sendAnswer got not ok status. Result:', await response.json());
-           throw new Error(`Network response was not ok - status ${response.status}`);
-        }
-
-        return await response.json();
-    } catch (error) {
-        console.error('Fetch error:', error);
-        throw new Error(`Ups, some error occurred during getTask: ${error.message}`);
+    if (!response.ok) {
+      console.error('Ugh, sendAnswer got not ok status. Result:', await response.json());
+      throw new Error(`Network response was not ok - status ${response.status}`);
     }
+
+    return await response.json();
+  } catch (error) {
+    console.error('Fetch error:', error);
+    throw new Error(`Ups, some error occurred during getTask: ${error.message}`);
+  }
 }
 
 export async function sendAnswer(answer, token) {
-    const url = `https://zadania.aidevs.pl/answer/${token}`;
-    const requestBody = {
-        answer,
-    };
+  const url = `https://zadania.aidevs.pl/answer/${token}`;
+  const requestBody = {
+    answer,
+  };
 
-    try {
-        const response = await fetch(url, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(requestBody),
-        });
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(requestBody),
+    });
 
-        if (!response.ok) {
-            console.error('Ugh, sendAnswer got not ok status. Result:', await response.json());
-            throw new Error(`Network response was not ok - status ${response.status}`);
-        }
-
-        return await response.json();
-    } catch (error) {
-        console.error('Fetch error:', error);
-        throw new Error(`Ups, some error occurred during sendAnswer: ${error.message}`);
+    if (!response.ok) {
+      console.error('Ugh, sendAnswer got not ok status. Result:', await response.json());
+      throw new Error(`Network response was not ok - status ${response.status}`);
     }
+
+    return await response.json();
+  } catch (error) {
+    console.error('Fetch error:', error);
+    throw new Error(`Ups, some error occurred during sendAnswer: ${error.message}`);
+  }
 }
 
 export async function exchangeQuestionIntoAnswer(question, token) {
@@ -83,8 +82,8 @@ export async function exchangeQuestionIntoAnswer(question, token) {
     });
 
     if (!response.ok) {
-       console.error('Ugh, sendAnswer got not ok status. Result:', await response.json());
-       throw new Error(`Network response was not ok - status ${response.status}`);
+      console.error('Ugh, sendAnswer got not ok status. Result:', await response.json());
+      throw new Error(`Network response was not ok - status ${response.status}`);
     }
 
     return await response.json();
